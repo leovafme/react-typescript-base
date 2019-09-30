3b4b0478aae199fdd7553a99420586a00a372ebd
POPA22\POPA22 <innova.programador5@opa.com.co>
2019-09-29 21:14:29 -0500

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { AppState } from './store';
+import { SystemState } from './store/system/types';
+
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
+interface AppProps {
+  system: SystemState;
+}
 
-export default App;
+class App extends React.Component<AppProps> {
+  render(): JSX.Element {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+            {this.props.system.userName}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: AppState): AppProps => ({
+  system: state.system
+});
+
+export default connect(mapStateToProps)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from './store';
+import './index.css';
+
+const store = configureStore();
+
+const Root = (): JSX.Element => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
